7c1dad31fd3adb36ae71176f014b77c368e7758b
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-03-06 17:52:10 -0300

    feat: add layout component for app

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import { Home } from './pages/home'
+import { Layout } from './pages/layout'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
   },
 ])
