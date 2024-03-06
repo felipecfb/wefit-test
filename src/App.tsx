a5322e537b0e193df3140e15938c0301d3a5ae83
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-03-06 17:49:14 -0300

    feat: adding react router dom and creating home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './routes'
+
 export function App() {
-  return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
