27d0c042af4591aea27bc6474f3d91a6e3b116db
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-03-07 17:09:17 -0300

    style: adding padding bottom to layout

--- src/pages/layout/styles.ts
+++ src/pages/layout/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   margin: 0 180px;
-  padding: 0 16px;
+  padding: 0 16px 16px;
 
   @media screen and (max-width: 768px) {
     margin: 0;
